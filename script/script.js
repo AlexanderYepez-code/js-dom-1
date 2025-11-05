37f04b52313b55b00808988e642e21ae435da089
AlexanderYepez-code <alexsaomn17@gmail.com>
2025-11-05 17:32:57 +0100

    feat: esercizio completo

--- script/script.js
+++ script/script.js
@@ -5,24 +5,28 @@ const titleElem = document.querySelector(".title-main");
 let imgLamp = document.querySelector("img");
 imgLamp.src = "./img/white_lamp.png";
 console.log(imgLamp);
+const containerElem = document.querySelector(".container")
 
 const brtLight = document.querySelector(".btn");
 
 console.log(titleElem, brtLight);
 
+let isOff = false;
+brtLight.addEventListener("click", function () {
+    if (isOff === true) {
+        brtLight.classList.remove("bg-red");
+        containerElem.classList.add("opacity")
+        imgLamp.src = "./img/white_lamp.png";
+        brtLight.innerText = "Accendimi"
+        titleElem.innerText = "Accendi la luce che vedi meglio"
 
-brtLight.addEventListener("click", function() {
-    const isOff = brtLight.classList.contains("bg-red")
-    
-    if(isOff === true){
-       brtLight.classList.remove("bg-red") ;
-       imgLamp.src = "./img/white_lamp.png";
-       brtLight.innerText = "Accendimi"
-    }else {
+    } else {
         brtLight.classList.add("bg-red")
-    imgLamp.src = "./img/yellow_lamp.png";
-    brtLight.innerText = "Spegnimi"
-    titleElem.innerText = "Spegni la luce"
+        imgLamp.src = "./img/yellow_lamp.png";
+        containerElem.classList.remove("opacity")
+        brtLight.innerText = "Spegnimi"
+        titleElem.innerText = "Spegni la luce che le bolette le pago io!!!"
 
     }
+    isOff = !isOff
 });
